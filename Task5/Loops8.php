a7ece68da01b7c5d6e5412cb277d6c2d5bab6172
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-08-30 16:18:35 +0530

    feat: Improve GCD calculation in Loops8.php
    The code changes in Loops8.php improve the calculation of the Greatest Common Divisor (GCD) of two numbers using a while loop. The previous implementation had a bug, and this update fixes it by correctly finding the GCD.

--- Task5/Loops8.php
+++ Task5/Loops8.php
@@ -5,13 +5,26 @@ using while loop. -->
 $firstNumber = readline("Enter the first number: ");
 $secondNumber = readline("Enter the second number: ");
 
-while ($firstNumber != $secondNumber) {
-    if ($firstNumber > $secondNumber) {
-        $firstNumber -= $secondNumber;
-    } else {
-        $secondNumber -= $firstNumber;
-    }
+// how to find the GCD of two numbers
+// 1. Find the smaller number
+// 2. Divide the larger number by the smaller number
+// 3. If the remainder is 0, then the smaller number is the GCD
+// 4. If the remainder is not 0, then the smaller number becomes the larger number and the remainder becomes the smaller number
+// 5. Repeat the process until the remainder is 0
+
+if ($firstNumber > $secondNumber) {
+    $smallerNumber = $secondNumber;
+    $largerNumber = $firstNumber;
+} else {
+    $smallerNumber = $firstNumber;
+    $largerNumber = $secondNumber;
 }
 
-echo "The Greatest Common Divisor is: " . $firstNumber;
+while ($largerNumber % $smallerNumber != 0) {
+    $remainder = $largerNumber % $smallerNumber;
+    $largerNumber = $smallerNumber;
+    $smallerNumber = $remainder;
+}
+
+echo "The Greatest Common Divisor of $firstNumber and $secondNumber is: $smallerNumber\n";
 ?>
