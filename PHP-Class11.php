3280cb7de10a1fc9775ac5540e15d31f66e99405
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-09-27 16:15:43 +0530

    Refactor PHP-Class11.php to display received names via GET, POST, and REQUEST methods

--- PHP-Class11.php
+++ PHP-Class11.php
@@ -1,13 +1,22 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['getName'])) {
     echo "Name received via GET: " . htmlspecialchars($_GET['getName']) . "<br>";
+    echo "<pre>";
+    print_r($_SERVER);
+    echo "</pre>";
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['postName'])) {
     echo "Name received via POST: " . htmlspecialchars($_POST['postName']) . "<br>";
+    echo "<pre>";
+    print_r($_SERVER);
+    echo "</pre>";
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_REQUEST['requestName'])) {
     echo "Name received via REQUEST: " . htmlspecialchars($_REQUEST['requestName']) . "<br>";
+    echo "<pre>";
+    print_r($_SERVER);
+    echo "</pre>";
 }
 ?>
